35c0a89c7fc69baa4fea60693a5cb5749f1d6248
YiJia 钰嘉 <yijiatan0208@gmail.com>
2023-11-13 16:12:04 +0800

    Favourite

---
+++ app/src/main/java/com/example/eventmaster/FavouriteClass.java
@@ -0,0 +1,38 @@
+package com.example.eventmanager;
+
+public class FavouriteClass {
+    int userID, eventID, favouriteID;
+
+    public FavouriteClass() {
+    }
+
+    public FavouriteClass(int favouriteID, int userID, int eventID) {
+        this.favouriteID = favouriteID;
+        this.userID = userID;
+        this.eventID = eventID;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public int getEventID() {
+        return eventID;
+    }
+
+    public void setEventID(int eventID) {
+        this.eventID = eventID;
+    }
+
+    public int getFavouriteID() {
+        return favouriteID;
+    }
+
+    public void setFavouriteID(int favouriteID) {
+        this.favouriteID = favouriteID;
+    }
+}
